4a950bd421d2c582cda3ef25afa820b3276ee226
Webteam <webteam@RMPC.nl>
2025-02-28 13:02:56 +0100

    cleanup

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import backstop from 'backstopjs';
 import bodyParser from 'body-parser';
 import puppeteer from 'puppeteer';
 import cors from 'cors';
-import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
 dotenv.config();
