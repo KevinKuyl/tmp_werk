8c050af15758709d64a6b103d5164a11a0f8638f
Webteam <webteam@RMPC.nl>
2025-03-04 10:55:49 +0100

    cors fuckery...

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Private-Network', 'true');
+    res.setHeader('Referrer-Policy', 'no-referrer-when-downgrade');
     next();
 });
 
